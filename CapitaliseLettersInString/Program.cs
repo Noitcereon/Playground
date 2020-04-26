2247d9741788fbdf1d0066e9cb7ab6e63c204e27
Thomas (Noit) <tba@live.dk>
2020-04-26 12:50:23 +0200

    Added new kata.

---
+++ CapitaliseLettersInString/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CapitaliseLettersInString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Worker worker = new Worker();
+            worker.Start();
+
+            Console.ReadKey();
+        }
+    }
+
+
+}
---
+++ CapitaliseLettersInString/Worker.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CapitaliseLettersInString
+{
+    #region Task Description
+    //Your task is to convert strings to how they would be written by Jaden Smith. The strings are actual quotes from Jaden Smith, but they are not capitalized in the same way he originally typed them.
+
+    //    Example:
+
+    // Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"
+    // Jaden - Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real"
+
+    //Link to Jaden's former Twitter account @officialjaden via archive.org
+    //      Fundamentals
+    //      Strings
+    //      Arrays
+    #endregion
+    public class Worker
+    {
+        public void Start()
+        {
+            
+        }
+
+    }
+}
