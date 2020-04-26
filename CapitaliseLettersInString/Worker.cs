0a9b78f2de663be01544af64cfa6be8d59ddb52e
Thomas (Noit) <tba@live.dk>
2020-04-26 12:51:04 +0200

    Addd link to kata.

--- CapitaliseLettersInString/Worker.cs
+++ CapitaliseLettersInString/Worker.cs
@@ -5,8 +5,9 @@ using System.Text;
 namespace CapitaliseLettersInString
 {
     #region Task Description
-    //Your task is to convert strings to how they would be written by Jaden Smith. The strings are actual quotes from Jaden Smith, but they are not capitalized in the same way he originally typed them.
-
+    //Your task is to convert strings to how they would be written by Jaden Smith. The strings are actual quotes from Jaden Smith,
+    // but they are not capitalized in the same way he originally typed them.
+    // https://www.codewars.com/kata/5390bac347d09b7da40006f6/train/csharp
     //    Example:
 
     // Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"
