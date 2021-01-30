37ac6aa9d5fe70c9b99866e55a0b163a67dd1ed4
Thomas (Noit) <tba@live.dk>
2021-01-31 00:20:43 +0100

    A small "animation" attempt.

---
+++ ConsoleAnimation/AnimationWorker.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleAnimation
+{
+    class AnimationWorker
+    {
+        public async void Start()
+        {
+            await Task.Run(PerformLoadAnimation);
+            Console.WriteLine("Done.");
+        }
+
+        private void PerformLoadAnimation()
+        {
+            int numberOfTimesToAnimate = 0;
+
+            while (numberOfTimesToAnimate <= 10)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    Console.Write('*');
+                    Thread.Sleep(200);
+                }
+                Console.Clear();
+                numberOfTimesToAnimate++;
+            }
+        }
+    }
+}
---
+++ ConsoleAnimation/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleAnimation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AnimationWorker animationWorker = new AnimationWorker();
+            animationWorker.Start();
+            Console.ReadKey();
+        }
+    }
+}
